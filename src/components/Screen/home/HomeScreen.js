41e4a37df5757b050d8130142b1866170ae94e5d
Leandro <leaxavier2014@gmail.com>
2021-12-25 00:21:39 -0500

    agregando banner random

--- src/components/Screen/home/HomeScreen.js
+++ src/components/Screen/home/HomeScreen.js
@@ -8,7 +8,7 @@ import { CardBestAnime } from '../../Cards/CardBestAnime';
 
 export const HomeScreen = ({...props}) => {
 
-    const [old, setOld] = useState({})
+    const [old, setOld] = useState([])
     
     useEffect(() => {
         getAnimeRetro()
@@ -16,39 +16,37 @@ export const HomeScreen = ({...props}) => {
 
    
     const getAnimeRetro = async() => {
-        const url = `https://api.aniapi.com/v1/anime/22`;
+        const url = `https://api.aniapi.com/v1/random/anime/1/true`;
         const response = await fetch(url);
         const {data} = await response.json()
 
-        const anime = {
-                id : data.id,
-                titleEsp : data.titles.en,
-                titleJap : data.titles.jp,
-                description: data.description,
-                score: data.score,
-                cover: data.cover_image,
-                banner: data.banner_image
+        const anime = data.map(resp => {
+            return {
+                id : resp.id,
+                titleEsp : resp.titles.en,
+                description: resp.description,
+                score: resp.score,
+                cover: resp.cover_image,
             }
-        
-
+        })
         setOld(anime)
-        
     }
   
     return (
-    
-           <div style={{backgroundColor:'black'}}>
-             
-                        <div> 
+        <>
+            {old.map(dat => (
                             <Card className="bg-dark text-white">
-                                <Card.Img src={old.banner} alt="Card image" />
+                                <Card.Img src={dat.cover} alt="Card image" style={{width:"90%", height: "500px"}}/>
                                     <Card.ImgOverlay>
-                                    <Card.Title style={{paddingTop: '20px', width:'600px'}}>{old.titleEsp}</Card.Title>
-                                    <Card.Text style={{paddingTop: '50px', width:'410px'}}>Puntuacion: {old.score}</Card.Text>
+                                    <Card.Title style={{paddingTop: '20px', width:'600px'}}>{dat.titleEsp}</Card.Title>
+                                    <Card.Text style={{paddingTop: '50px', width:'410px'}}>Puntuacion: {dat.score}</Card.Text>
                                     </Card.ImgOverlay>
                                  
                             </Card>
-                        </div>
+                ))
+            }
+                        
+                
                    <div style={{float:'right'}}>
                        <h1 style={{textAlign:'center', marginTop:'40px', paddingBottom:'15px', color:'white'}}>Peliculas canceladas</h1>
                    <CardMovie/>
@@ -63,7 +61,9 @@ export const HomeScreen = ({...props}) => {
                    <h1 style={{ textAlign:'center', marginTop:'100px', paddingBottom:'30px',color:'white'}}>mejores animes 2021</h1>
                    <CardBestAnime/>
                </div>
-           </div>
-        
+           
+    
+           
+           </>
     )
 }
